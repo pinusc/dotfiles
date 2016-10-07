67fb3e63296e641a6f9ff4772ae54a0e24a94cfa
pinusc <giuseppe.stelluto@gmail.com>
2016-10-07 14:19:59 +0200

    Implement stalonetray support

--- bin/show_conky.sh
+++ bin/show_conky.sh
@@ -12,6 +12,7 @@ RUNNING=$(ps aux|grep -v awk|awk '/conky -c dotfiles\/conkyrc1/ {print $2}'|wc -
 if [ $RUNNING -ne "0" ]; then
 	#killall conky
   kill $(ps aux|grep -v awk|awk '/conky -c dotfiles\/conkyrc1/ {print $2}')
+  pkill stalonetray
   bspc config right_padding 0
 else
   # bspc query returns the number of monitors
@@ -19,4 +20,5 @@ else
   # this assures conky is always on the rightmost
   bspc config -m $(bspc query -M | tail -c 2) right_padding 143
   conky -c dotfiles/conkyrc1 &> /dev/null
+  stalonetray &
 fi
