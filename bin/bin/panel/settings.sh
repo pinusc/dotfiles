3dc0332eb04e2e2d0078518530e2ebe2f52dcd79
Giuseppe Stelluto <giuseppe.stelluto@gmail.com>
2018-02-04 02:57:08 +0800

    Cleaned panel
    - Moved bar/dzen2 to panel/dzen2
    - Modev the settings variables to a new standalone file
    - Fixed a bug in the dzen scripts

---
+++ bin/bin/panel/settings.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+export PANEL_FIFO=/tmp/panel-fifo
+export PANEL_FIFO_2=/tmp/panel-fifo-2
+export CLOCK_FIFO=/tmp/panel-clock-fifo
+export PANEL_HEIGHT=36
+export PANEL_WIDTH=1910
+export PANEL_GAP=5
+export PANEL_Y=5
+# export PANEL_FONT_FAMILY="Source Code Pro for Powerline:size=12"
+export PANEL_FONT_FAMILY="-gohu-*-medium-*-*-*-14-*-*-*-*-*-*-*"
+export PANEL_FONT_FAMILY="Inconsolata for Powerline:size=14"
+export # PANEL_FONT_FAMILY="Monofur for Powerline:size=15"
+export ICON_FONT="FontAwesome:size=14"
+export ICON_FONT_2="Weather Icons:size=14"
+export dzencommand_music="$HOME/bin/panel/dzen2/scripts/mpdzen"
+export dzencommand_calendar="zsh $HOME/bin/panel/dzen2/calendar"
+export dkeyboard="$HOME/bin/dkeyboard"
+export MAILDIR="$HOME/Maildir/School/INBOX/new"
+export MAILCOMMAND="thunderbird"
+# export ICON_FONT2="fontcustom:size=17"
+# export PANEL_FIFO PANEL_HEIGHT PANEL_FONT_FAMILY
