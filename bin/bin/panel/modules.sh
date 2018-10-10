2d722e5d1ffefee12beebc1d44497421aa66b208
pinusc <giuseppe.stelluto@gmail.com>
2018-10-10 02:56:41 +0200

    Move all icons to separate file; switch to NerdFont; various fixes
    Weather was fixed in an hilarious bug involving locale

--- bin/bin/panel/modules.sh
+++ bin/bin/panel/modules.sh
@@ -7,41 +7,41 @@ clock() {
 
 calendar() {
     url_comm="firefox calendar.google.com"
-    echo "D%{A:$dzencommand_calendar:}%{A3:$url_comm:}\uf073 $(date +'%a %b %d')%{A}%{A}"
+    echo "D%{A:$dzencommand_calendar:}%{A3:$url_comm:}$IC_CALENDAR $(date +'%a %b %d')%{A}%{A}"
 }
 
 alsa_volume() {
     ALSA_STATE=1
     VOLUME=$(ponymix get-volume)
-    ICON='\uf028'
+    ICON=$IC_VOLUME_MAX
     if [ $ALSA_STATE ]; then
         if [[ $VOLUME -ge 70 ]]
         then
-            ICON='\uf028'
+            ICON=$IC_VOLUME_MAX
         fi
         if [[ $VOLUME -gt 0 && $VOLUME -lt 70 ]]
         then
-            ICON='\uf027'
+            ICON=$IC_VOLUME_MAX
         fi
         if [[ $VOLUME -eq 0 ]]
         then
-            ICON='\uf026'
+            ICON=$IC_VOLUME_MAX
         fi
     fi
     echo "V%{A:pavucontrol:}$ICON $VOLUME%{A}"
 }
 
 getip() {
-    echo -e I'\uf041' $(curl -s icanhazip.com)
+    echo -e I$IC_IP $(curl -s icanhazip.com)
     sleep 10
 }
 
 mail() {
     count=$(find "$MAILDIR" -type f | grep -vE ',[^,]*S[^,]*$' | wc -l)
     if [ $count -gt 0 ]; then
-        echo "Mf%{A3:$FETCHMAILCOMMAND:}%{A:$MAILCOMMAND:}\uf0e0 $count%{A}${A}"
+        echo "Mf%{A3:$FETCHMAILCOMMAND:}%{A:$MAILCOMMAND:}$IC_MAIL $count%{A}${A}"
     else
-        echo "M0%{A3:$FETCHMAILCOMMAND:}%{A:MAILCOMMAND:}\uf0e0%{A}%{A}"
+        echo "M0%{A3:$FETCHMAILCOMMAND:}%{A:MAILCOMMAND:}$IC_MAIL%{A}%{A}"
     fi
 }
 
@@ -55,7 +55,7 @@ pollution() {
     aqi=$(echo $res  | jq -r .data.aqi)
     url=$(echo $res  | jq -r .data.city.url)
     # url_comm="firefox \"${url#*://}\"" # the colon in the URL will interfere with lemonbar's syntax and this is easier than figuring out how to escape ti
-    icon="\uf075"
+    icon=$IC_POLLUTION
     color=""
     if (( $(echo $aqi '<=' 50 | bc -l) )); then
         color="g"
@@ -75,52 +75,58 @@ pollution() {
 }
 
 weather() {
-    weather_response=$(curl "https://api.darksky.net/forecast/$API_DARKSKY/31.6035,120.7391?units=si")
+    export weather_response=$(curl "https://api.darksky.net/forecast/$API_DARKSKY/31.6035,120.7391?units=si")
     url_comm="firefox darksky.net/forecast/changshu/si12/en"
     apiicon=$(echo $weather_response | jq -r .currently.icon)
-    temperature=$(echo $weather_response | jq -r .currently.temperature | bc -l)
+    temperature=$(echo $weather_response | jq -r .currently.temperature)
+
+    # I use en_DK as my locale, which separates decimals with a comma
+    # But the api uses a point
+    # So when printf is given a decimal number using a point, it gives error and fucks up
+    # Setting LC_NUMERIC to C ensures this never happens
+    LC_NUMERIC="C" 
+    temperature_int=$(printf "%.0f" "$temperature")
     temperature=$(printf "%.1f" "$temperature")
     icon=""
     color=""  # depending on temperature, can be blue, yellow or red
     case $apiicon in
         clear-day)
-            icon="\uf30d"
+            icon=$IC_WEATHER_CLEAR
             ;;
         clear-night)
-            icon="\uf32b"
+            icon=$IC_WEATHER_CLEAR_NIGHT
             ;;
         rain)
-            icon="\uf319"
+            icon=$IC_WEATHER_RAIN
             ;;
         cloudy)
-            icon="\uf312"
+            icon=$IC_WEATHER_CLOUDY
             ;;
         partly-cloudy-day)
-            icon="\uf302"
+            icon=$IC_WEATHER_PARTLY_CLOUDY_DAY
             ;;
         partly-cloudy-night)
-            icon="\uf32e"
+            icon=$IC_WEATHER_PARTLY_CLOUDY_NIGHT
             ;;
         snow)
-            icon="\uf31a"
+            icon=$IC_WEATHER_SNOW
             ;;
         wind)
-            icon="\uf34b"
+            icon=$IC_WEATHER_WIND
             ;;
         fog)
-            icon="\uf31e"
+            icon=$IC_WEATHER_FOG
             ;;
     esac
-    if (( $(echo $temperature '<' 15 | bc -l) )); then
+    if [ $temperature_int -lt 15 ]; then
         color="b"
-    elif (( $(echo 15 '<' $temperature '&&' $temperature '<' 30 | bc -l) )); then
+    elif [ 15 -lt $temperature_int ] && [ $temperature_int -lt 30 ]; then
         color="y"
-    elif (( $(echo $temperature '>' 30 | bc -l) )); then
+    elif [ $temperature_int -gt 30 ]; then
         color="r"
     fi
     # echo -e F"$color%{A:$url_comm:}$icon $temperature%{A}"
     echo -e F"$color$icon $temperature"
-    #curl "https://api.darksky.net/forecast/cdd9e613e163fa9768e2a4d3b3219ca6/31.6035,120.7391?exclude=minutely,hourly,daily,alerts,flags" > /tmp/weather
 }
 
 check_connection() {
@@ -192,9 +198,9 @@ music() {
         if [[ $(echo $(mpc status)| awk '/volume/ {print $2}') != "n/a" ]]; then
             if [[ -n $(mpc status | grep paused) ]]
             then
-                echo "R%{T3}%{A:mpc prev:}\uf048%{A} %{A:mpc play:}\uf04b%{A}  %{A:mpc next:}\uf051%{A}%{T1} $SONG_NAME"
+                echo "R%{T3}%{A:mpc prev:}$IC_MUSIC_PREV%{A} %{A:mpc play:}$IC_MUSIC_PLAY%{A}  %{A:mpc next:}$IC_MUSIC_NEXT%{A}%{T1} $SONG_NAME"
             else
-                echo "R%{T3}%{A:mpc prev:}\uf048%{A} %{A:mpc pause:}\uf04c%{A} %{A:mpc next:}\uf051%{A}%{T1} %{A:$dzencommand_music:}$SONG_NAME%{A} "
+                echo "R%{T3}%{A:mpc prev:}$IC_MUSIC_PREV%{A} %{A:mpc pause:}$IC_MUSIC_PAUSE%{A} %{A:mpc next:}$IC_MUSIC_NEXT%{A}%{T1} %{A:$dzencommand_music:}$SONG_NAME%{A} "
             fi
         fi
     fi
@@ -211,10 +217,10 @@ musicp() {
             if [[ -n $(mpc status | grep paused) ]]
             then
                 command="play"
-                icon="\uf04b"
+                icon=$IC_MUSIC_PLAY
             else
                 command="pause"
-                icon="\uf04c"
+                icon=$IC_MUSIC_PAUSE
             fi
             echo "m%{A:mpc $command:}%{A3:$dzencommand_music:}$icon%{A}%{A}"
             # echo "m%{A:mpc $command:}$icon%{A}"
