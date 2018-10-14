81f5e72ac51c2120d4d64602c3eca615349e2508
Giuseppe Stelluto <giuseppe.stelluto@gmail.com>
2018-10-14 18:11:14 +0200

    Make font-size a variable and panel-height dyamic

--- bin/bin/panel/settings.sh
+++ bin/bin/panel/settings.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 export TMPDIR=$(mktemp -d)
-export PANEL_HEIGHT=45
 # dim=$(xdpyinfo | awk '/dimensions:/ { print $2; exit }')
 dim=$(xrandr | awk '/.*primary/ { print $4; exit }')
 dimx=${dim%x*}
 export PANEL_GAP=$(bspc config window_gap)
 export PANEL_WIDTH=$(( dimx - 2 * PANEL_GAP))
-export PANEL_FONT_FAMILY="Gohu GohuFont:size=11"
-export ICON_FONT="GohuFont Nerd Font:size=11"
+FONT_SIZE=11
+export PANEL_HEIGHT=$(( FONT_SIZE * 4 ))
+export PANEL_FONT_FAMILY="Gohu GohuFont:size=$FONT_SIZE"
+export ICON_FONT="GohuFont Nerd Font:size=$FONT_SIZE"
 export dzencommand_music="$HOME/bin/panel/dzen2/scripts/mpdzen"
 export dzencommand_calendar="zsh $HOME/bin/panel/dzen2/calendar"
 export dkeyboard="$HOME/bin/dkeyboard"
