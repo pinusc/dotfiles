40a3edc91f749712e40241ae218f09ed85f11157
pinusc <giuseppe.stelluto@gmail.com>
2019-01-16 03:01:09 +0100

    Add mapping to insert em-dash
    Switch to us-intl-altgr for hack to work

--- bin/bin/toggle_dvorak.sh
+++ bin/bin/toggle_dvorak.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(setxkbmap -query | grep -q "layout:\s\+it") && setxkbmap us || setxkbmap it;
+(setxkbmap -query | grep -q "layout:\s\+it") && setxkbmap -layout 'us(altgr-intl)' || setxkbmap it
 xmodmap ~/.Xmodmap
