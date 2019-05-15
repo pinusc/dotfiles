3495fd225d80d295149f00cf441e82bb4826b29e
Giuseppe Stelluto <giuseppe.stelluto@gmail.com>
2019-05-15 02:11:07 +0200

    Automatically switch qutebrowser's theme

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,8 +2,8 @@
 # Documentation:
 #   qute://help/configuring.html
 #   qute://help/settings.html
+import os
 
-# Uncomment this to still load settings configured via autoconfig.yml
 config.load_autoconfig()
 
 # Enable JavaScript.
@@ -17,3 +17,84 @@ config.set('content.javascript.enabled', True, 'chrome://*/*')
 # Enable JavaScript.
 # Type: Bool
 config.set('content.javascript.enabled', True, 'qute://*/*')
+
+if os.path.exists('/home/pinusc/.local/share/colors'):
+    with open('/home/pinusc/.local/share/colors') as f:
+        source = f.read()
+        exec(source)
+
+    c.colors.completion.fg = base05
+    c.colors.completion.odd.bg = base00
+    c.colors.completion.even.bg = base00
+    c.colors.completion.category.fg = base0A
+    c.colors.completion.category.bg = base00
+    c.colors.completion.category.border.top = base00
+    c.colors.completion.category.border.bottom = base00
+    c.colors.completion.item.selected.fg = base01
+    c.colors.completion.item.selected.bg = base0A
+    c.colors.completion.item.selected.border.top = base0A
+    c.colors.completion.item.selected.border.bottom = base0A
+    c.colors.completion.match.fg = base0B
+    c.colors.completion.scrollbar.fg = base05
+    c.colors.completion.scrollbar.bg = base00
+    c.colors.downloads.bar.bg = base00
+    c.colors.downloads.start.fg = base00
+    c.colors.downloads.start.bg = base0D
+    c.colors.downloads.stop.fg = base00
+    c.colors.downloads.stop.bg = base0C
+    c.colors.downloads.error.fg = base08
+    c.colors.hints.fg = base00
+    c.colors.hints.bg = base0A
+    c.colors.hints.match.fg = base05
+    c.colors.keyhint.fg = base05
+    c.colors.keyhint.suffix.fg = base05
+    c.colors.keyhint.bg = base00
+    c.colors.messages.error.fg = base00
+    c.colors.messages.error.bg = base08
+    c.colors.messages.error.border = base08
+    c.colors.messages.warning.fg = base00
+    c.colors.messages.warning.bg = base0E
+    c.colors.messages.warning.border = base0E
+    c.colors.messages.info.fg = base05
+    c.colors.messages.info.bg = base00
+    c.colors.messages.info.border = base00
+    c.colors.prompts.fg = base05
+    c.colors.prompts.border = base00
+    c.colors.prompts.bg = base00
+    c.colors.prompts.selected.bg = base0A
+    c.colors.statusbar.normal.fg = base0B
+    c.colors.statusbar.normal.bg = base00
+    c.colors.statusbar.insert.fg = base00
+    c.colors.statusbar.insert.bg = base0D
+    c.colors.statusbar.passthrough.fg = base00
+    c.colors.statusbar.passthrough.bg = base0C
+    c.colors.statusbar.private.fg = base00
+    c.colors.statusbar.private.bg = base03
+    c.colors.statusbar.command.fg = base05
+    c.colors.statusbar.command.bg = base00
+    c.colors.statusbar.command.private.fg = base05
+    c.colors.statusbar.command.private.bg = base00
+    c.colors.statusbar.caret.fg = base00
+    c.colors.statusbar.caret.bg = base0E
+    c.colors.statusbar.caret.selection.fg = base00
+    c.colors.statusbar.caret.selection.bg = base0D
+    c.colors.statusbar.progress.bg = base0D
+    c.colors.statusbar.url.fg = base05
+    c.colors.statusbar.url.error.fg = base08
+    c.colors.statusbar.url.hover.fg = base05
+    c.colors.statusbar.url.success.http.fg = base0C
+    c.colors.statusbar.url.success.https.fg = base0B
+    c.colors.statusbar.url.warn.fg = base0E
+    c.colors.tabs.bar.bg = base00
+    c.colors.tabs.indicator.start = base0D
+    c.colors.tabs.indicator.stop = base0C
+    c.colors.tabs.indicator.error = base08
+    c.colors.tabs.odd.fg = base05
+    c.colors.tabs.odd.bg = base00
+    c.colors.tabs.even.fg = base05
+    c.colors.tabs.even.bg = base00
+    c.colors.tabs.selected.odd.fg = base05
+    c.colors.tabs.selected.odd.bg = base02
+    c.colors.tabs.selected.even.fg = base05
+    c.colors.tabs.selected.even.bg = base02
+
