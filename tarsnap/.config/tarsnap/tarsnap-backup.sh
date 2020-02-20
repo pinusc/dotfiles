ad3a6934c799890ad42c5296f5e9706a93624b1d
pinusc <giuseppe.stelluto@gmail.com>
2020-02-20 16:13:29 -0500

    Improve tarsnap patters, add systemd timer

--- tarsnap/.config/tarsnap/tarsnap-backup.sh
+++ tarsnap/.config/tarsnap/tarsnap-backup.sh
@@ -5,7 +5,7 @@ tarsnap_output_filename="$(mktemp /tmp/tarsnap-out-XXXX.log)"
 if ! /bin/tarsnap -v -c \
     -f "docs-$(uname -n | cut -d. -f1)-$(date --iso-8601)" \
     --configfile "$HOME/.config/tarsnap/tarsnaprc" \
-    "$HOME/docs" >"$tarsnap_output_filename" 2>&1
+    "/data/docs" >"$tarsnap_output_filename" 2>&1
 then
     mail -s "Tarsnap backup FAILURE" "$email" < "$tarsnap_output_filename"
 fi
