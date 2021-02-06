f21287e5b44ef1c44c10c7918bad0a41b6a576ff
pinusc <giuseppe.stelluto@gmail.com>
2021-02-06 05:03:15 -0500

    Add warnings before hibernating

--- bin/bin/util/auto_hibernate.sh
+++ bin/bin/util/auto_hibernate.sh
@@ -17,8 +17,16 @@ raw_charge=$(acpi | sed "s/[^,]\\+\?, //; s/%.\\+//; s/%//")
 raw_charge=$(acpi | sed "s/[^,]\\+\?, //; s/%.\\+//; s/%//")
 bcharge=$(( (raw_charge - BATTERY_ZERO) * 100 / (100 - BATTERY_ZERO) ))
 
+if (( bcharge - THRESHOLD < 5 )); then
+    remaining=$(( bcharge - THRESHOLD ))
+    which notify-send 2>/dev/null && notify-send -u critical "Battery depleting. Only $remaining% of charge left until hibernating."
+fi
+
 if [ "$bcharge" -le "$THRESHOLD" ]; then
-    echo hib
+    which notify-send 2>/dev/null && notify-send -u critical "Battery depleted. Hibernating in 30 seconds..."
+    sleep 15
+    which notify-send 2>/dev/null && notify-send -u critical "Battery depleted. Hibernating in 15 seconds..."
+    sleep 15
     systemctl "$ACTION"
     exit 0
 fi
