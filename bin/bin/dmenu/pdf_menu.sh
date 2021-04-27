f95d512b48e56c61049b7acdb70201cfe11ee4eb
pinusc <giuseppe.stelluto@gmail.com>
2021-04-27 16:45:29 -0400

    Create an awesome new "open_pdf" command

---
+++ bin/bin/dmenu/pdf_menu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [[ "$1" = "-h" || "$1" = "--help" ]]; then
+    cat<<EOF
+    Usage: apdf [search-term]
+    Find and open a pdf file from ~/docs, ~/books, ~/downloads, ~/tmp, with rofi
+EOF
+fi
+
+set -Eeuo pipefail
+
+FOLDERS=(books docs downloads tmp)
+FORMATS=(-e pdf -e epub)
+
+cd "$HOME"
+fname="$(cd "$HOME"; fd --base-directory "$HOME" -I . -e "${FORMATS[@]}" "${FOLDERS[@]}" | rofi -dmenu -i)"
+
+if [[ -f "$fname" ]]; then
+    "$PDF_VIEWER" "$fname" &
+    disown
+else
+    exit 1
+fi
