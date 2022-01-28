b3cb3f23e36e0b1a60f54566a6145c19d217dbb7
pinusc <giuseppe.stelluto@gmail.com>
2022-01-28 16:34:11 -0500

    Add config for new monito

--- bin/bin/util/dock_handler.sh
+++ bin/bin/util/dock_handler.sh
@@ -29,23 +29,41 @@ esac
 if [[ "$DOCKED" = 1 ]]; then
     echo "docky docked"
     xrandr --auto
-    monitor=$(xrandr --listmonitors | awk '{ print $4; }' | grep '.' | grep -v 'LVDS1' | head -n 1)
-    case "$monitor" in
-        HDMI2)
-            xrandr --newmode "1366x768"x0.0   69.30  1366 1404 1426 1436  768 771 777 803 -hsync -vsync
-            xrandr --addmode HDMI2 "1366x768"x0.0
-            xrandr --output HDMI2 --right-of LVDS1 --mode "1366x768"x0.0 --gamma 1 --output LVDS1 --primary
-            ;;
-        VGA1)
-            echo "VGA"
-            xrandr --output VGA1 --right-of LVDS1 --output LVDS1 --primary
-            ;;
-    esac
+    # Here nmonitors is the number of _external_ displays
+    nmonitors=$(xrandr --listmonitors | awk '{ print $4; }' | grep '.' | grep -v 'LVDS' | wc -l)
+    # monitor is (hopefully) the primary external display
+    monitor=$(xrandr --listmonitors | awk '{ print $4; }' | grep '.' | grep -v 'LVDS' | head -n 1)
+    echo $nmonitors
+    if [ "$nmonitors" -eq 2 ]; then
+        # special config for three monitors
+        xrandr --output VGA1 --auto
+        xrandr --output HDMI2 --auto
+        xrandr --output HDMI2 --right-of LVDS1 --output VGA1 --right-of HDMI2
+    else
+        case "$monitor" in
+            DP2)
+                # remember to set primary correctly for bar reasons
+                xrandr --output DP2 --primary --mode --mode 2560x1080
+                xrandr --output LVDS1 --right-of DP2
+                ;;
+            HDMI2)
+                xrandr --newmode "1366x768"x0.0   69.30  1366 1404 1426 1436  768 771 777 803 -hsync -vsync
+                xrandr --addmode HDMI2 "1366x768"x0.0
+                xrandr --output HDMI2 --right-of LVDS1 --mode "1366x768"x0.0 --gamma 1 --output LVDS1 --primary
+                ;;
+            VGA1)
+                echo "VGA"
+                xrandr --output VGA1 --right-of LVDS1 --output LVDS1 --primary
+                ;;
+        esac
+    fi
 else
     echo "docky undocked"
     xrandr --auto
-    "$XDG_CONFIG_HOME"/bspwm/bspc_monitors.sh
+    # remember to set primary correctly for bar reasons
+    xrandr --output LVDS1 --primary
 fi
+"$XDG_CONFIG_HOME"/bspwm/bspc_monitors.sh
 killall panel; panel &>/dev/null &
 disown
 ~/bin/util/wallpaper/random_wallpaper.sh
