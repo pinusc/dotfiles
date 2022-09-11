ed8ba990b0d74062218179208496bb8edf65be69
pinusc <giuseppe.stelluto@gmail.com>
2022-09-10 22:03:57 -0400

    Add cleanpdf script

---
+++ bin/bin/util/cleanpdf.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] FILENAME
+
+Extract pages from a scanned PDF to clean it in scantailor.
+
+EOF
+  exit
+}
+
+filename=""
+
+
+args=("$@")
+if [[ ${#args[@]} -eq 0 ]]; then
+    echo "Missing script arguments" >&2
+    exit 1
+fi
+
+case "$1" in
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    *)
+        filename="$1"
+        ;;
+esac
+
+
+tempdir="$(mktemp --tmpdir -d cleanpdf-XXXX)"
+encoding="$(pdfimages -list "$filename" | awk '(NR==3){ print $9; }')"
+if [[ "$encoding" = "ccitt" ]]; then
+    pdfimages -tiff "$filename" "$tempdir/"
+else
+    pdfimages -all "$filename" "$tempdir/"
+fi
+
+echo "$tempdir" | xclip -i -selection clipboard
+echo "$tempdir"
+
+scantailor 
