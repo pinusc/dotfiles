ffcbc11b0388790df62f6b53c2f4ac26f723aaff
pinusc <giuseppe.stelluto@gmail.com>
2022-09-10 22:08:53 -0400

    Add screen-off command; fix formatting

--- bin/bin/dmenu/power_menu.sh
+++ bin/bin/dmenu/power_menu.sh
@@ -1,23 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 source ~/.dmenurc
 
-COMMANDS="
-suspend\n
-poweroff\n
-reboot\n
-hibernate\n
-hybrid-sleep\n
-halt\n
-lock\n
-"
+COMMANDS="suspend
+poweroff
+reboot
+hibernate
+hybrid-sleep
+halt
+lock
+screen-off"
 # dmenu_run -h $h -w $w -l 1 -q -p "                         execute" -fn "$font" -nb "#2d2d2d" -nf "#dedede" -sb "#2d2d2d" -sf "#8b9dc3"
     # dmenu -p "Power:" -fn "$FONT" -nb $BGCOL -nf $FGCOL -sb $SELBGCOL -sf $SELFGCOL)
 
-command=$(echo -e $COMMANDS | $dmenu_command -p "power:")
+echo -e "$COMMANDS"
+
+command="$(echo -e "$COMMANDS" | $dmenu_command -p "power:")"
 [ "$command" = "" ] && exit 1
-if [[ "${command#?}" = "lock" ]]; then
+echo $command
+if [[ "${command}" = "lock" ]]; then
     lock
+elif [[ "${command}" = "screen-off" ]]; then
+    sleep 1
+    xset dpms force off
 else
     systemctl $command
 fi
