e41fc58df66ef98a6ea6870d1d1c31ae08163f6a
pinusc <giuseppe.stelluto@gmail.com>
2022-09-16 01:30:46 -0400

    Make script production-ready

--- bin/bin/util/tea-timer.pl
+++ bin/bin/util/tea-timer.pl
@@ -5,27 +5,54 @@ use warnings;
 use feature 'say';
 use Scalar::Util qw(looks_like_number);
 use Term::ANSIColor;
+use Getopt::Long qw(:config auto_help);
+use Pod::Usage;
+# use Text::ProgressBar::Bar;
+# use Curses;
 
 $| = 1;  # flush every print, not on newlines
 
 my $baseline = 0;
 my $increment = 0;
+my $prestart = 3;
+my $colorbehavior = "auto";
+my $colorenabled = "auto";
+my $soundenabled = 1;
+
+GetOptions(
+    "time=i" => \$baseline,
+    "increment=i" => \$increment,
+    "prestart:i" => \$prestart,
+    "sound!" => \$soundenabled,
+    "color:s" => \$colorbehavior,
+    "help|?" => sub { pod2usage(-exitval => 0, -verbose => 1) },
+) or pod2usage(2);
 
 if (@ARGV == 2)  {
     $baseline = $ARGV[0];
     $increment = $ARGV[1];
 } elsif (@ARGV == 1)  {
     $baseline = $ARGV[0];
-} elsif (@ARGV == 1 && $ARGV[0] eq "-h") {
-    # TODO ask for values
-    print "Usage: tea-timer.pl [BASELINE [INCREMENT]]\n\nTime tea steep, with support for time increments on subsequent steeps.\n";
-    exit(0);
+}
+
+if ($colorbehavior eq "auto") {
+    $ENV{ANSI_COLORS_DISABLED} = ! -t STDOUT;
+} elsif ($colorbehavior eq "always") {
+    $ENV{ANSI_COLORS_DISABLED} = 0;
+} elsif ($colorbehavior eq "never") {
+    $ENV{ANSI_COLORS_DISABLED} = 1;
 } else {
+    print qq(Invalid option --color $colorbehavior. Valid options are "auto", "never", "always"\n\n);
+    pod2usage(-verbose => 1, -exitval => 2);
+}
+
+unless ($baseline) {
     print "How many seconds should your tea steep? ";
     my $r = <STDIN>;
     chomp $r;
     $baseline = $r;
 }
+
 
 sub seconds2str {
     my $total = shift;
@@ -48,31 +75,49 @@ sub notify {
     clear();
     say colored(["green"], "Time's up!");
     say "You can pu'er that tea now";
-    qx(notify-send "Your tea is ready!" >/dev/null 2>&1);
-    qx(play -q /usr/share/sounds/freedesktop/stereo/complete.oga >/dev/null 2>&1);
+    if ($soundenabled) {
+        qx(play -q /usr/share/sounds/freedesktop/stereo/complete.oga >/dev/null 2>&1);
+    }
+    qx(notify-send -u critical "Your tea is ready!" "You can pu'er it now" >/dev/null 2>&1);
 }
 
 sub countdown {
     my $time = shift;
+    my $text = shift;
+    my $elapsed = 0;
+    # my $progress = Text::ProgressBar->new(maxval => $time);
+    # $progress::maxval = $time;
+    # $progress->start();
     # hide the cursor
     clear();
     print "\e[?25l"; # hide cursor
     do  {
-        my $digits = length($time + 1);
-        my $timestr = seconds2str($time);
-        print "\rTime remaining: " . colored(["yellow"], "$timestr   ");
+        my $digits = length($time - $elapsed + 1);
+        my $timestr = seconds2str($time - $elapsed);
+        print "\r$text" . colored(["yellow"], "$timestr   ");
+        # $progress->update($elapsed);
         # print "\b" x $digits . "\b";
         sleep 1;
-    } while (--$time > 0);
-    notify();
+    } while (++$elapsed < $time);
+    # $progress->finish();
     print "\e[?25h\n"; # show cursor again
 }
 
-my $iter = 0;
+# timer is a countdown with final alert
+sub timer {
+    countdown @_;
+    notify();
+}
+
+
+my $timeval = $baseline;
 while (1) {
-    countdown($baseline + $iter * $increment);
+    if ($prestart) {
+        countdown($prestart, "Starting in... ");
+    }
+    timer($timeval, "Time remaining: ");
     print colored(["bright_black"], "Press q to stop\n");
-    print "Last steep was " . colored(["yellow"], seconds2str($baseline + $iter * $increment)) . "\n";
+    print "Last steep was " . colored(["yellow"], seconds2str($timeval)) . "\n";
     print "Increment for next steep? [" . colored(["green"], $increment) . "]:  " . color("green");
     my $r = <STDIN>;
     chomp $r;
@@ -84,7 +129,7 @@ while (1) {
     } elsif (!$r eq "") {
         last ;
     }
-    $iter++;
+    $timeval += $increment;
 }
 
 use sigtrap 'handler' => \&sigtrap, 'HUP', 'INT','ABRT','QUIT','TERM';
@@ -108,3 +153,46 @@ sub sigtrap(){
 
 # # Display the notification
 # $notification->show();
+ 
+__END__
+
+=head1 NAME
+
+tea-timer.pl - A simple terminal gongfu tea timer 
+
+=head1 SYNOPSIS
+
+tea-timer.pl [options] [TIME [INCREMENT]]
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<TIME>
+
+Length of time, in seconds, the timer should count. 
+If not given as an argument, it will be prompted on screen.
+If B<INCREMENT> is non-zero, ...
+
+=item B<--prestart|-p>[=VALUE]
+
+By default, tea-timer.pl starts a 3s timer before the actual countdown, to give
+some time to actually pour water and start steeping.
+If --prestart is passed without arguments, this behavior is turned off.
+If VALUE is given, set the pre-start timer to VALUE seconds. 
+
+=item B<--color>[=VALUE]
+
+If VALUE is "auto", colorize output only if the output device is a terminal, and print uncolorized output if it is a pipe, file, or otherwise.
+If VALUE is "never", always produce uncolorized output.
+If VALUE is "always", always produce colorized output.
+
+The default behavior is "auto".
+
+=item B<--no-sound>
+
+Disable playing a sound notification.
+
+=back
+
+=cut
