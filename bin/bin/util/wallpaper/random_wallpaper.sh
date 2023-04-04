9d1a09b0ddef94ad76b58207142317cd54a2dc61
Giuseppe Stelluto <giuseppe@gstelluto.com>
2023-04-04 13:40:50 -0400

    scripts/random_wallpaper: Do not display restrict

--- bin/bin/util/wallpaper/random_wallpaper.sh
+++ bin/bin/util/wallpaper/random_wallpaper.sh
@@ -4,7 +4,6 @@ ALIST=( `ls -w 1 $WALLPAPERS` )
 RANGE=${#ALIST[*]}
 SHOW=$(( $RANDOM % $RANGE ))
 #DISPLAY=:0.0 feh --bg-fill $WALLPAPERS/${ALIST[$SHOW]}
-export DISPLAY=:0.0
-DISPLAY=:0 /bin/feh --recursive --randomize --bg-fill $WALLPAPERS
+/bin/feh --recursive --randomize --bg-fill $WALLPAPERS
 #feh --recursive --randomize --bg-fill /home/pinusc/.wallpaper
 #/bin/bash -c 'ln -s $(find /home/pinusc/.wallpaper | sort -R | head -n 1) /tmp/wallpaper'
