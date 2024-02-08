3380adff36c7aa7faa20db588cfb3ee4e1454d0a
Giuseppe Stelluto <giuseppe@gstelluto.com>
2024-02-08 05:06:32 +0100

    tarsnap: notify on failure, saner service

--- tarsnap/.config/tarsnap/tarsnap-backup.sh
+++ tarsnap/.config/tarsnap/tarsnap-backup.sh
@@ -7,10 +7,10 @@ if ! /bin/tarsnap -v -c \
     -f "docs-$(uname -n | cut -d. -f1)-$(date --iso-8601)" \
     --configfile "$HOME/.config/tarsnap/tarsnaprc" \
     -X "$HOME/.config/tarsnap/exclude-patterns" \
-    "/data/docs" #>"$tarsnap_output_filename" 2>&1
+    "/data/docs" >"$tarsnap_output_filename" 2>&1
 then
+    notify-send -u critical "Tarsnap backup failure" "See journalctl log" >/dev/null 2>&1 &
     mail -s "Tarsnap backup FAILURE" "$email" < "$tarsnap_output_filename"
     excode=1
 fi
-rm "$tarsnap_output_filename"
 exit "$excode"
