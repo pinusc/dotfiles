625bd3ecc84210a6c5ec36a2fbd905684f91a189
Giuseppe Stelluto <giuseppe@gstelluto.com>
2025-06-24 23:31:18 +0200

    add hyprland

---
+++ hypr/.config/hypr/hyprpaper-shuf.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+set -x
+
+readarray -t wallpapers < <(find ~/.wallpaper -type f | shuf)
+
+n=0
+hyprctl monitors -j | jq -r '.[] | .name' | while IFS= read -r monitor; do
+    (( n += 1 ))
+    hyprctl hyprpaper preload "${wallpapers[n]}"
+    hyprctl hyprpaper wallpaper "$monitor,${wallpapers[n]}"
+done
