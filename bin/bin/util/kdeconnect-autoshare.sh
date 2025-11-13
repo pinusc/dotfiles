c27f88f836d81720883811165306f373808c3240
Giulia Stelluto <giulia@gstelluto.com>
2025-11-13 18:44:18 +0100

    bin: kdeconnect-autoshare.sh: add --help

--- bin/bin/util/kdeconnect-autoshare.sh
+++ bin/bin/util/kdeconnect-autoshare.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+if [ "$1" = "--help " ] || [ "$1" = "-h" ] || [ "$1" = "" ]; then
+    echo "Usage:"
+    echo "  kdeconnect-autoshare.sh FILE"
+    echo "  kdeconnect-autoshare.sh --help"
+    exit 1
+fi
+
+
 to_share="$1"
 device_line="$(kdeconnect-cli --list-available --id-name-only | rofi -dmenu -auto-select -e "Device to send to?")"
 device_id="$(echo "$device_line" |  cut -d' ' -f1)"
